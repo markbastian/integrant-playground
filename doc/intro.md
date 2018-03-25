34489ec9430cc6bc2572b3e4d7c7c76e409a2688
Mark Bastian <markbastian@gmail.com>
2018-03-25 08:27:31 -0600

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to integrant-playground
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject integrant-playground "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [integrant "0.6.3"]
+                 [com.github.fakemongo/fongo "2.1.0"]
+                 [com.novemberain/monger "3.1.0"]
+                 [datascript "0.16.4"]
+                 [org.immutant/web "2.1.10"]
+                 [ring/ring-core "1.6.3"]
+                 [clj-http "3.8.0"]])
